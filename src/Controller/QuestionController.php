44e1e78d31a08bdaaa35ae3f3a549d76cd39aff8
abdounikarim <abdounikarim@gmail.com>
2020-09-02 12:02:24 +0200

    Generate URLs

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class QuestionController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
