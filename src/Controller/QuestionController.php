330e660b7757b8275caaa845fc217851d1928d35
abdounikarim <abdounikarim@gmail.com>
2020-09-03 14:16:00 +0200

    Controllers: Boring, Beautiful Services

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Service\MarkdownHelper;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,6 +11,15 @@ use Twig\Environment;
 
 class QuestionController extends AbstractController
 {
+    private $logger;
+    private $isDebug;
+
+    public function __construct(LoggerInterface $logger, bool $isDebug)
+    {
+        $this->logger = $logger;
+        $this->isDebug = $isDebug;
+    }
+
     /**
      * @Route("/", name="app_homepage")
      */
@@ -30,7 +40,9 @@ class QuestionController extends AbstractController
      */
     public function show($slug, MarkdownHelper $markdownHelper)
     {
-        dump($this->getParameter('cache_adapter'));
+        if ($this->isDebug) {
+            $this->logger->info('We are in debug mode!');
+        }
 
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still 🤣',
