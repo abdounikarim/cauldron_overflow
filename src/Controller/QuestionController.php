5d07400320df0a5a1e86f8b5f008adfa0657fcd4
abdounikarim <abdounikarim@gmail.com>
2020-09-03 14:43:39 +0200

    Using & Overriding Secrets

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Service\MarkdownHelper;
 use Psr\Log\LoggerInterface;
-use Sentry\State\HubInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -39,15 +38,11 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownHelper $markdownHelper, HubInterface $sentryHub)
+    public function show($slug, MarkdownHelper $markdownHelper)
     {
-        dump($sentryHub);
-
         if ($this->isDebug) {
             $this->logger->info('We are in debug mode!');
         }
-
-        throw new \Exception('bad stuff happened!');
 
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still 🤣',
